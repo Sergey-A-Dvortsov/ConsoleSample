116eba483de6a45c03c3547caeb1a87dd3059dfe
Synapse <sergey.a.dvortsov@gmail.com>
2026-01-02 05:57:26 +0300

    Any changes (#2)
    * Any changes
    
    * Any changes two
    
    ---------
    
    Co-authored-by: Дворцов Сергей Аркадьевич <sergey_gavrilov@list.ru>

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("First commit");
-Console.WriteLine("Local branch test");
+Console.WriteLine("Local branch test"); 
+Console.WriteLine("Any changes");
+Console.WriteLine("Any changes two");
